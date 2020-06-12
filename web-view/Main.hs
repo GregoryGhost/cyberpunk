be140eaed6bac280397951cbf0367eb4cc96b05a
Gregory <lobster287@gmail.com>
2020-06-12 18:49:54 +0700

    Formatted hs files in web view

--- web-view/Main.hs
+++ web-view/Main.hs
@@ -1,29 +1,24 @@
--- | Haskell language pragma
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE CPP #-}
 
--- | Haskell module declaration
 module Main where
 
--- | Miso framework import
-import           Miso
-import           Miso.String
-
--- | JSAddle import
 #ifndef __GHCJS__
 import           Language.Javascript.JSaddle.Warp as JSaddle
 import qualified Network.Wai.Handler.Warp         as Warp
 import           Network.WebSockets
 #endif
-import           Control.Monad.IO.Class
+import Control.Monad.IO.Class
+import Miso
+import Miso.String
 
 -- | Type synonym for an application model
 data Model = Model
-  {
-    name :: String
-    ,number :: Int
-  } deriving (Show, Eq)
+  { name :: String,
+    number :: Int
+  }
+  deriving (Show, Eq)
 
 -- | Sum type for application events
 data Action
@@ -48,30 +43,34 @@ main :: IO ()
 main = runApp $ startApp App {..}
   where
     initialAction = SayHelloWorld -- initial action to be executed on application load
-    model  = Model { number = 0, name = "kek" }                    -- initial model
-    update = updateModel          -- update function
-    view   = viewModel            -- view function
-    events = defaultEvents        -- default delegated events
-    subs   = []                   -- empty subscription list
-    mountPoint = Nothing          -- mount point for application (Nothing defaults to 'body')
+    model = Model {number = 0, name = "kek"} -- initial model
+    update = updateModel -- update function
+    view = viewModel -- view function
+    events = defaultEvents -- default delegated events
+    subs = [] -- empty subscription list
+    mountPoint = Nothing -- mount point for application (Nothing defaults to 'body')
 
 -- | Updates model, optionally introduces side effects
 updateModel :: Action -> Model -> Effect Action Model
-updateModel AddOne m = noEff m2 where 
-  n = number m
-  m2 = m { number = n + 1 }
-updateModel SubtractOne m = noEff m2 where 
-  n = number m
-  m2 = m { number = n - 1 }
+updateModel AddOne m = noEff m2
+  where
+    n = number m
+    m2 = m {number = n + 1}
+updateModel SubtractOne m = noEff m2
+  where
+    n = number m
+    m2 = m {number = n - 1}
 updateModel NoOp m = noEff m
 updateModel SayHelloWorld m = m <# do
   liftIO (putStrLn "Hello World") >> pure NoOp
 
 -- | Constructs a virtual DOM from a model
 viewModel :: Model -> View Action
-viewModel x = div_ [] [
-   button_ [ onClick AddOne ] [ text "++" ]
- , text (ms $ number x)
- , text (ms $ name x)
- , button_ [ onClick SubtractOne ] [ text "-" ]
- ]
+viewModel x =
+  div_
+    []
+    [ button_ [onClick AddOne] [text "++"],
+      text (ms $ number x),
+      text (ms $ name x),
+      button_ [onClick SubtractOne] [text "-"]
+    ]
